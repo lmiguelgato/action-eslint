b4774b3c039ebdb010b4f4386022652de3b1e78a
Luis M. Gato <lmiguelgato@gmail.com>
2021-04-07 11:55:43 -0500

    Concatenate path to glob

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,12 @@ import { exec } from '@actions/exec';
 import { getInput, setFailed } from '@actions/core';
 
 async function run() {
-	try {
-		const customGlob = getInput('custom-glob');
+	try {		
+		var customGlob = getInput('custom-glob');
 		const customPath = getInput('custom-path');
+		if (customPath) {
+			customGlob = customPath + '/' + customGlob
+		}
 		console.log(`##[add-matcher]${join(__dirname, '..', '.github', 'eslint-stylish.json')}`);
 		const args = [`${join(process.cwd(), customPath, 'node_modules/eslint/bin/eslint')}`, '--ext', 'js,jsx,ts,tsx', customGlob];
 		await exec('node', args);
